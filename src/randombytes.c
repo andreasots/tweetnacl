dfc8fd3618541bc89b9ec9686e9273d1f677294c
Andreas Ots <andreasots@gmail.com>
2014-07-02 18:46:01 +0300

    fix compile errors

--- src/randombytes.c
+++ src/randombytes.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "tweetnacl.h"
 
+typedef unsigned char u8;
+typedef unsigned long long u64;
+
 void randombytes(u8* buf, u64 len)
 {
     fprintf(stderr, "tweetnacl called `randombytes', aborting");
--- src/randombytes.rs
+++ src/randombytes.rs
@@ -1,5 +1,3 @@
-use libc;
-use std;
 use std::rand::Rng;
 use std::rand::os::OsRng;
 use std::kinds::marker;
